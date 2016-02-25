c392bcfa6689a0b01fcb17018c25d792d3774af3
Josh Charles <josh.charles@gmail.com>
2016-02-24 19:47:58 -0800

    updated script so I can push it and hopefully trigger jenkins

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# so it turns out this isn't really needed, because there is an almost duplicate script already written in /scripts
+# I'm just going to commit this to attempt to get jenkins to pick up the push.
 pushd vault-ui
 npm install
 gulp
