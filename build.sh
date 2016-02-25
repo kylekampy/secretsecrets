10f37e45ac2cee6d484d29db049367b1643533bd
Josh Charles <josh.charles@gmail.com>
2016-02-24 20:16:53 -0800

    jenkins ping

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # so it turns out this isn't really needed, because there is an almost duplicate script already written in /scripts
 # I'm just going to commit this to attempt to get jenkins to pick up the push.
+# jenkins ping
 pushd vault-ui
 npm install
 gulp
